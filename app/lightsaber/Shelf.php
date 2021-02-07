51285a8b7e21f33d3416e1c6deea2d60c83c4264
Timothy Gross <mitcoding@gmail.com>
2021-02-06 22:08:00 -0800

    php cucumber working with lightsaber example

---
+++ app/lightsaber/Shelf.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\lightsaber;
+
+final class Shelf
+{
+	private $priceMap = array();
+
+	public function setProductPrice($product, $price) {
+		$this->priceMap[$product] = $price;
+	}
+
+	public function getProductPrice($product) {
+		return $this->priceMap[$product];
+	}	
+}
+
+?>
---
+++ tests/features/bootstrap/FeatureContext.php
@@ -0,0 +1,66 @@
+<?php
+
+use App\lightsaber\Shelf;
+use App\lightsaber\Basket;
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use PHPUnit\Framework\Assert;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+	    $this->shelf = new Shelf();
+	    $this->basket = new Basket($this->shelf);
+    }
+
+    /**
+     * @Given there is a :product, which costs £:price
+     */
+    public function thereIsAWhichCostsPs($product, $price)
+    {
+	    $this->shelf->setProductPrice($product, floatval($price));
+    }
+
+    /**
+     * @When I add the :product to the basket
+     */
+    public function iAddTheToTheBasket($product)
+    {
+	    $this->basket->addProduct($product);
+    }
+
+    /**
+     * @Then I should have :count products in the basket
+     */
+    public function iShouldHaveProductInTheBasket($count)
+    {
+	Assert::assertCount(
+            intval($count),
+            $this->basket
+        );
+    }
+
+    /**
+     * @Then the overall basket price should be £:price
+     */
+    public function theOverallBasketPriceShouldBePs($price)
+    {
+	Assert::assertSame(
+            floatval($price),
+            $this->basket->getTotalPrice()
+        );
+    }
+}
